1d23d763e86a39c560347b8d0c3812fa1561bfe3
Nichole Davidson <nicholedavidson@Nicholes-MacBook-Pro.local>
2022-11-11 20:56:17 -0700

    Added synchronization to FlavorRequestQueue class. Tests pass.

--- ThreadSafety/IceCream/src/main/java/com/kenzie/threadsafety/icecream/FlavorRequestQueue.java
+++ ThreadSafety/IceCream/src/main/java/com/kenzie/threadsafety/icecream/FlavorRequestQueue.java
@@ -5,23 +5,23 @@ import com.kenzie.threadsafety.icecream.model.Flavor;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class FlavorRequestQueue {
+public final class FlavorRequestQueue {
     private final Queue<Flavor> flavorQueue;
 
     public FlavorRequestQueue() {
         flavorQueue = new LinkedList<>();
     }
 
-    public void needFlavor(Flavor flavor) {
+    public synchronized void needFlavor(Flavor flavor) {
         flavorQueue.add(flavor);
     }
 
     public Flavor nextNeededFlavor() {
-        Flavor flavor = flavorQueue.poll();
+        Flavor flavor = pollFlavor(flavorQueue);
         while (flavor == null) {
             try {
                 Thread.sleep(10L);
-                flavor = flavorQueue.poll();
+                flavor = pollFlavor(flavorQueue);
             } catch (InterruptedException e) {
                 System.out.println("!!!Interrupted waiting for flavor request!!!");
                 e.printStackTrace();
@@ -29,6 +29,10 @@ public class FlavorRequestQueue {
             }
         }
         return flavor;
+    }
+
+    public synchronized Flavor pollFlavor(Queue<Flavor> flavors) {
+        return flavors.poll();
     }
 
     public int requestCount() {
